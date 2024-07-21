50a1e474d906a70f54cf7fcf1f6f467a4571e355
jsalvasoler <jsalvasoler@hotmail.com>
2024-07-21 17:12:17 +0200

    feat: model improvements and save model

--- src/der_die_das/train.py
+++ src/der_die_das/train.py
@@ -8,7 +8,7 @@ import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
 
-from der_die_das.utils import DATA_DIR
+from der_die_das.utils import DATA_DIR, MODEL_DIR
 
 
 class TransformerClassifier(nn.Module):
@@ -18,7 +18,7 @@ class TransformerClassifier(nn.Module):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embed_dim)
         self.positional_encoding = nn.Parameter(torch.zeros(1, max_length, embed_dim))
-        encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=num_heads)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=num_heads, batch_first=True)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
         self.fc = nn.Linear(embed_dim, num_classes)
 
@@ -27,6 +27,10 @@ class TransformerClassifier(nn.Module):
         x = self.transformer_encoder(x)
         x = x.mean(dim=1)  # Global average pooling
         return self.fc(x)
+
+    def save_model(self) -> None:
+        timestamp = pd.Timestamp.now().strftime("%Y%m%d%H%M%S")
+        torch.save(self.state_dict(), os.path.join(MODEL_DIR, f"model_{timestamp}.pt"))
 
 
 class GermanNouns(Dataset):
@@ -72,16 +76,22 @@ def train() -> None:
     model = TransformerClassifier(vocab_size, embed_dim, num_heads, num_layers, max_length, num_classes)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
     for epoch in range(50):
-        for batch_words, batch_labels in dataloader:
-            batch_words_tensor = torch.tensor(batch_words)
-            batch_labels_tensor = torch.tensor(batch_labels)
+        running_loss = 0.0
 
+        for batch_words, batch_labels in dataloader:
             optimizer.zero_grad()
-            outputs = model(batch_words_tensor)
-            loss = criterion(outputs, batch_labels_tensor)
+            outputs = model(torch.tensor(batch_words))
+            loss = criterion(outputs, torch.tensor(batch_labels))
             loss.backward()
             optimizer.step()
 
-        print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+            running_loss += loss.item()
+
+        scheduler.step()
+
+        print(f"Epoch {epoch + 1}, Running loss: {running_loss / len(dataloader)}, Loss: {loss.item()}")
+
+    model.save_model()
--- src/der_die_das/utils.py
+++ src/der_die_das/utils.py
@@ -3,3 +3,4 @@ import os
 this_directory = os.path.dirname(__file__)
 
 DATA_DIR = os.path.join(this_directory, "..", "..", "data")
+MODEL_DIR = os.path.join(this_directory, "..", "..", "models")
