643ae606e512ef81a1cf8aba112a0d1e252d282a
jsalvasoler <jsalvasoler@hotmail.com>
2024-07-21 14:55:29 +0200

    fix process_data call

--- src/der_die_das/__main__.py
+++ src/der_die_das/__main__.py
@@ -12,7 +12,7 @@ def cli() -> None:
 @click.option("--rerun_raw_to_processed", is_flag=True, help="Whether to rerun the raw to processed step")
 def process_data_command(*, rerun_raw_to_processed: bool) -> None:
     click.echo("Running data processing pipeline")
-    process_data(rerun_raw_to_processed)
+    process_data(rerun_raw_to_processed=rerun_raw_to_processed)
 
 
 if __name__ == "__main__":
