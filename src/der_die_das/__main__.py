2dbfb83138ceeffe9b8e22147e36d0ab3ca903ac
jsalvasoler <jsalvasoler@hotmail.com>
2024-07-30 20:02:58 +0200

    refactor: improve CLI with defaults and arguments

--- src/der_die_das/__main__.py
+++ src/der_die_das/__main__.py
@@ -4,6 +4,7 @@ import click
 
 from der_die_das.data_processing import process_data
 from der_die_das.evaluate import evaluate
+from der_die_das.model import DEFAULT_SETTINGS
 from der_die_das.train import train
 from der_die_das.utils import LANGUAGES
 
@@ -14,7 +15,7 @@ def cli() -> None:
 
 
 @cli.command(name="process_data", help="Run the data processing pipeling")
-@click.option("--language", help="The language to process the data for", type=click.Choice(LANGUAGES), required=True)
+@click.argument("language", type=click.Choice(LANGUAGES))
 @click.option("--rerun_raw_to_processed", is_flag=True, help="Whether to rerun the raw to processed step")
 @click.option("--rerun_train_test_split", is_flag=True, help="Whether to rerun the train test split step")
 def process_data_command(language: str, *, rerun_raw_to_processed: bool, rerun_train_test_split: bool) -> None:
@@ -23,22 +24,47 @@ def process_data_command(language: str, *, rerun_raw_to_processed: bool, rerun_t
 
 
 @cli.command(name="train", help="Train the model")
-@click.option("--language", help="The language to train the model for", type=click.Choice(LANGUAGES), required=True)
-@click.option("--batch_size", type=int, help="The batch size for training")
-@click.option("--lr", type=float, help="The learning rate for training")
-@click.option("--step_size", type=int, help="The step size for the learning rate scheduler")
-@click.option("--gamma", type=float, help="The gamma for the learning rate scheduler")
-@click.option("--num_epochs", type=int, help="The number of epochs for training")
+@click.argument("language", type=click.Choice(LANGUAGES))
+@click.option(
+    "--batch_size",
+    type=int,
+    help="Batch size for training",
+    default=DEFAULT_SETTINGS["batch_size"],
+    show_default=True,
+)
+@click.option(
+    "--lr", type=float, help="The learning rate for training", default=DEFAULT_SETTINGS["lr"], show_default=True
+)
+@click.option(
+    "--step_size",
+    type=int,
+    help="Step size for the learning rate scheduler",
+    default=DEFAULT_SETTINGS["step_size"],
+    show_default=True,
+)
+@click.option(
+    "--gamma",
+    type=float,
+    help="Gamma for the learning rate scheduler",
+    default=DEFAULT_SETTINGS["gamma"],
+    show_default=True,
+)
+@click.option(
+    "--num_epochs",
+    type=int,
+    help="Number of epochs for training",
+    default=DEFAULT_SETTINGS["num_epochs"],
+    show_default=True,
+)
 @click.option(
     "--val_size",
     type=float,
-    help="The size of the validation set as a fraction of the training set."
-    " If not provided, no validation set is used",
+    help="Size of the validation set as a fraction of the training set." " If not provided, no validation set is used",
 )
 @click.option(
     "--early_stop",
     type=int,
-    help="The number of epochs to wait before stopping training if the validation loss does not decrease."
+    help="Number of epochs to wait before stopping training if the validation loss does not decrease."
     " If not provided, early stopping is not used",
 )
 def train_command(
@@ -61,7 +87,7 @@ def train_command(
 @cli.command(name="evaluate", help="Evaluate the model")
 @click.option(
     "--model_time_stamp",
-    help="The timestamp of the model to evaluate. If not provided, the latest model will be evaluated",
+    help="Timestamp of the model to evaluate. If not provided, the latest model will be evaluated",
 )
 def evaluate_command(model_time_stamp: str | None) -> None:
     click.echo("Evaluating the model")
